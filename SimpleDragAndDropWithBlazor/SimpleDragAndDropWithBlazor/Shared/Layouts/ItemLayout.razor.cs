81acdc3a6abdd3dddb1fa0af62a1f08e07100e32
Paul Cook <1893351+Simcon@users.noreply.github.com>
2021-09-14 14:15:42 +0100

    ItemLayout control

---
+++ SimpleDragAndDropWithBlazor/SimpleDragAndDropWithBlazor/Shared/Layouts/ItemLayout.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SimpleDragAndDropWithBlazor.Shared.Layouts
+{
+    public partial class ItemLayout : ComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
